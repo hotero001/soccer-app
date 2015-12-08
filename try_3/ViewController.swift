07f399e00e86a65fa8ca6b94d38dfc541b434a81
Hector Otero <otero111@hotmail.com>
2015-12-08 04:13:46 -0800

    added all project files

--- try_3/ViewController.swift
+++ try_3/ViewController.swift
@@ -13,10 +13,43 @@ class ViewController: UIViewController {
     @IBOutlet weak var textField: UITextField!
     @IBOutlet weak var segueButton: UIButton!
     @IBOutlet weak var awayTeamTextField: UITextField!
+    @IBOutlet weak var halfLengthTextField: UITextField!
+    @IBOutlet weak var homeColorTextField: UITextField!
+    @IBOutlet weak var awayColorTextField: UITextField!
+    @IBOutlet weak var ar1TextField: UITextField!
+    @IBOutlet weak var ar2TextField: UITextField!
+    @IBOutlet weak var refTextField: UITextField!
+    @IBOutlet weak var startTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        halfLengthTextField.placeholder = "Half Length(minutes)"
+        halfLengthTextField.textAlignment = NSTextAlignment.Center
+        
+        textField.placeholder = "Home Team Name"
+        textField.textAlignment = NSTextAlignment.Center
+        
+        awayTeamTextField.placeholder = "Away Team Name"
+        awayTeamTextField.textAlignment = NSTextAlignment.Center
+        
+        homeColorTextField.placeholder = "Home Team Color"
+        homeColorTextField.textAlignment = NSTextAlignment.Center
+        
+        awayColorTextField.placeholder = "Away Team Color"
+        awayColorTextField.textAlignment = NSTextAlignment.Center
+        
+        ar1TextField.placeholder = "AR #1 Name"
+        ar1TextField.textAlignment = NSTextAlignment.Center
+        
+        ar2TextField.placeholder = "AR #2 Name"
+        ar2TextField.textAlignment = NSTextAlignment.Center
+        
+        refTextField.placeholder = "Center Ref Name"
+        refTextField.textAlignment = NSTextAlignment.Center
+        
+        startTextField.placeholder = "Game Start Time"
+        startTextField.textAlignment = NSTextAlignment.Center
     }
 
     override func didReceiveMemoryWarning() {
@@ -29,8 +62,8 @@ class ViewController: UIViewController {
             secondVC.firstVCText = textField.text!
         
             secondVC.awayTeam = awayTeamTextField.text!
+        
+            secondVC.halfTimeLength = halfLengthTextField.text!
     }
-
-
 }
 
--- try_3/secondViewController.swift
+++ try_3/secondViewController.swift
@@ -13,19 +13,59 @@ class SecondViewController: UIViewController{
     
     @IBOutlet weak var textFromFirstLabel: UILabel!
     @IBOutlet weak var awayTeamLabel: UILabel!
+    @IBOutlet weak var timerLabel: UILabel!
+    
     
     var firstVCText = ""
     var awayTeam = ""
+    var halfTimeLength = ""
     
+    var counter:Int?
+    //var counter = Int(halfTimeLength)
+    var timer = NSTimer()
+
     override func viewDidLoad() {
         super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
         textFromFirstLabel.text = firstVCText
         awayTeamLabel.text = awayTeam
+        timerLabel.text = halfTimeLength
+        counter = Int(halfTimeLength)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func startTimer(sender: AnyObject){
+        if !timer.valid{
+            var view = UIView()
+            view = sender as! UIView
+            view.userInteractionEnabled = true
+            timer = NSTimer.scheduledTimerWithTimeInterval(1.0, target: self, selector: "countDown", userInfo: nil, repeats: true)
+            view.userInteractionEnabled = false
+        }
+    }
+
+    
+    @IBAction func stopTimer(sender: AnyObject) {
+        timer.invalidate()
+        //let time = Int(halfTimeLength)
+        //counter = Int(halfTimeLength)!
+    }
+    
+    func countDown() {
+        counter = counter! - 1
+        if (counter == 0){
+            counter = 0
+        }
+        updateText()
+    }
+    
+    func updateText(){
+        let _:Int?
+        timerLabel.text = String(counter!)
+    }
+    
 }
